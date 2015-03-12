3414ec0cc77fa792903bde88c99fa3a81c32f754
40075949 <mhuynh01@qub.ac.uk>
2015-03-12 15:31:37 +0000

    Tuan- added enhanceContrast_PowerLaw, reduceNoisebyConvolver, reduceNoiseReductionbyMedian methods to PreProcessor class. and manualThresholdAnImage, open, close methods to PostProcessor class.

--- src/PostProcessor.java
+++ src/PostProcessor.java
@@ -11,4 +11,31 @@ public class PostProcessor
 	{
 		return image;
 	}
+	
+    
+    private BufferedImage manualThresholdAnImage(BufferedImage image)
+    {
+    	short[] lut = new short[256];
+		for(int i = 0; i < (short)lut.length; i++)
+		{
+			if(i <= t)
+				lut[i] = 255;
+			else
+				lut[i]= 0;
+		}
+		
+    	return ImageOp.pixelop(image, lut);
+    }
+    
+    private BufferedImage open(BufferedImage image, int m)
+    {
+    	// m is mask size.
+    	return ImageOp.open(image, 3);
+    }
+    
+    private BufferedImage close(BufferedImage image, int m)
+    {
+    	//m is mask size.
+    	return ImageOp.close(image, 3);
+    }
 }
--- src/PreProcessor.java
+++ src/PreProcessor.java
@@ -16,8 +16,10 @@ public class PreProcessor
 		processedImage = enhanceBrightness(processedImage);
 		processedImage = enhanceContrast(processedImage);
 		
-		// Reduce noise.
-		processedImage = reduceNoise(processedImage);
+		// Reduce noise by Covolver or Median.
+		processedImage = reduceNoisebyConvolver(processedImage);
+		
+		
 		
 		// Return the processed image.
 		return processedImage;
@@ -34,10 +36,25 @@ public class PreProcessor
 		return enhanceContrast_LinearStretch(image, 1f, 0f);
 	}
 	
-	private BufferedImage reduceNoise(BufferedImage image)
+	private BufferedImage reduceNoisebyConvolver(BufferedImage image)
 	{
-		return image;
+		//declare mask.
+		final float[] LOWPASS5X5 = {1/9.f,1/9.f,1/9.f,
+									1/9.f,1/9.f,1/9.f,
+									1/9.f,1/9.f,1/9.f,
+									1/9.f,1/9.f,1/9.f,
+									1/9.f,1/9.f,1/9.f};
+    	
+    	//return noise reduce image.
+    	return ImageOp.convolver(image, LOWPASS5X5);
 	}
+	
+    
+    private BufferedImage reduceNoiseReductionbyMedian(BufferedImage source)
+    {
+    	//return noise reduce image.
+    	return ImageOp.median(source, 2);
+    }
 
 	private BufferedImage enhanceContrast_LinearStretch(BufferedImage image, float m, float c)
 	{	
@@ -45,6 +62,19 @@ public class PreProcessor
 		short[] LUT = new short[256];
 		for(int i = 0; i < LUT.length; i++)
 			LUT[i] = (short) Math.min(Math.max((m * i + c), 0), 255);
+		
+		// Return the processed image.
+		return ImageOp.pixelop(image, LUT);
+	}
+	
+	public BufferedImage enhanceContrast_PowerLaw(BufferedImage image)
+	{
+		// Create the lookup table.
+		short[] lut = new short[256];
+		for(int i = 0; i < lut.length; i++)
+		{
+			lut[i] = (short) (Math.pow(i,gamma)/ Math.pow(255,gamma-1));
+		}
 		
 		// Return the processed image.
 		return ImageOp.pixelop(image, LUT);
