71b1c62f042ae9a3acd36f776584a617061b2807
Glen Brown <gbrown92@outlook.com>
2015-03-20 21:15:56 +0000

    Glen - Redid the LDF classifier.

--- src/ClassifierLinearDiscriminantFunction.java
+++ src/ClassifierLinearDiscriminantFunction.java
@@ -1,80 +1,56 @@
+import java.awt.geom.Point2D;
 import java.util.ArrayList;
 
 
 public class ClassifierLinearDiscriminantFunction implements IClassifier{
 
-	private double[][] trainingVectors;
+	private double m_pb, c;
 
-	public ClassifierLinearDiscriminantFunction() {
-
-		
+	public ClassifierLinearDiscriminantFunction() 
+	{
 	}
 	
-	@Override
-	public void train(ArrayList<TrainingVector> vectors, boolean isVerbose) {
+	public void train(ArrayList<TrainingVector> vectors, boolean isVerbose) 
+	{
+		// Store the two mean points of the two classes.
+		Point2D.Double vG_mu = new Point2D.Double();
+		Point2D.Double vH_mu = new Point2D.Double();
 		
-		// Store the vectors.
-		trainingVectors = new double[vectors.size()][vectors.get(0).getVector().length];
-				
-		for(int i = 0; i < vectors.size(); i++)
-		{
-			trainingVectors[i] = vectors.get(i).getVector();
-		}
+		int numVG = 0;
+		int numVH = 0;
 		
+		// Iterate over the training vectors and get the mean.
+		for(TrainingVector tVec : vectors)
+			if(tVec.isPositive())
+			{
+				vG_mu.setLocation(vG_mu.x + tVec.getVector()[0], vG_mu.y + tVec.getVector()[1]);
+				numVG++;
+			}
+			else
+			{
+				vH_mu.setLocation(vH_mu.x + tVec.getVector()[0], vH_mu.y + tVec.getVector()[1]);
+				numVH++;
+			}
 		
+		// Get the averages.
+		vG_mu.setLocation(vG_mu.x / numVG, vG_mu.y / numVG);
+		vH_mu.setLocation(vH_mu.x / numVH, vH_mu.y / numVH);
+		
+		// Get the gradient of the line connecting the two means.
+		double m = (vG_mu.y - vH_mu.y) / (vG_mu.x - vH_mu.x);
+		
+		// Get the gradient perpendicular to this line.
+		m_pb = -1/m;
+		
+		// Get the midpoint of this line.
+		Point2D.Double midPoint = new Point2D.Double((vG_mu.x + vH_mu.x) / 2, (vG_mu.y + vH_mu.y) / 2);
+		
+		// Get c.
+		c = midPoint.y - m_pb * midPoint.x;
+	}	
+
+	public boolean predict(double[] vector) 
+	{
+		return (vector[1] - m_pb * vector[0] - c) > 0;
 	}
-	
-	
-
-	@Override
-	public boolean predict(double[] vector) {
-		
-		// Store the mean vectors and absolute distances.
-		double[][] meanVecs = new double[trainingVectors.length][trainingVectors[0].length];
-		
-		//find the mean
-		double [] meanFeaure = new double[trainingVectors.length];
-		
-		for(int i = 0; i < meanVecs.length; i ++)
-		{
-			int count= 0;
-			for(int j = 0; j < meanVecs.length; j++)
-				{
-					meanFeaure[i] += meanVecs[i][j];
-					count++;
-				}
-			meanFeaure[i] = meanFeaure[i]/count;
-		}
-		
-		
-		double meanVector = 0;
-		for(int i =0; i < vector.length; i++)
-			meanVector += vector[i];
-		
-		meanVector = meanVector / vector.length;
-		
-		//find the gradients of line				
-		double m = 0;
-		m = (meanVector - meanFeaure[0])/(meanVector-meanFeaure[1]);
-		
-		//find the gradients of perpendicular bisector
-		double mpb = 0;
-		mpb = -1/m;
-		
-		
-		
-		//find the coordinate of the midpoints
-		double mp1 = (meanVector + meanFeaure[0])/2;
-		double mp2 = (meanVector + meanFeaure[1])/2;
-		
-		
-		//find c
-		double c = mp2 - (mpb*mp1);
-
-		if(mp2 + (mpb*mp1) - c > 0)
-			return true;
-		else
-			return false;
-	}
-
 }
