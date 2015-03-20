cce36a980f7d69d351b67888af9d7b5d1fb2d531
Glen Brown <gbrown92@outlook.com>
2015-03-20 14:43:20 +0000

    Glen - Actually commited the SegmenterAlphaTester...

---
+++ src/SegmenterAlphaTester.java
@@ -0,0 +1,53 @@
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
+
+
+public class SegmenterAlphaTester {
+
+	/**
+	 *  The name of the file containing the application properties.
+	 */
+	private final static String PROPERTY_FILENAME = "config.properties";
+
+	public static void main(String[] args) 
+	{
+		// Get the application properties.
+		Properties props = getProperties();
+
+		for(double i = -2; i <= 2; i+= 0.1)
+		{
+			// Get an instance of the Automated Image Recognition System
+			AIRSystem airs = new AIRSystem(i);
+
+			// Train the system using the training image set.
+			airs.train(props.getProperty("trainingPath"), false);
+
+			// Classify all the test images.
+			System.out.printf("\nAlpha %3.1f ", i);
+			airs.classifyAllInFolder(props.getProperty("testingPath"), false);
+		}
+	}
+
+	/**
+	 * Gets the application properties such as paths to training / testing folders.
+	 * @return The properties set in the config.properties file.
+	 */
+	public static Properties getProperties()
+	{
+		Properties props = new Properties();
+		InputStream inputStream =  IIS_G08.class.getClassLoader().getResourceAsStream(PROPERTY_FILENAME);
+
+		// Load the properties.
+		try
+		{
+			props.load(inputStream);
+		}
+		catch(IOException ex)
+		{
+			System.err.println(ex.getMessage());
+		}
+
+		return props;
+	}
+}
